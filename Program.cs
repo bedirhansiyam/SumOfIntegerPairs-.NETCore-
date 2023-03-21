8bd3093ea354b00cc563b64aba4c3552e545c0f8
Bedirhan <bedirhansiyam@gmail.com>
2023-03-21 16:39:48 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace SumOfIntegerPairs;
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool isInt = true;
+        List<int> numbers = new List<int>();
+
+        Console.Write("Please enter space-separated integer pairs : ");
+
+        do
+        {
+            string[] strNumbers = Console.ReadLine().Split(' ');
+
+            if(strNumbers.Count() % 2 != 0)
+            {
+                Console.WriteLine("");
+                Console.Write("Please only enter numbers in pairs and integers : ");
+                isInt = false;
+            }
+            else
+            {
+                for (int i = 0; i < strNumbers.Count(); i++)
+                {
+                    isInt = int.TryParse(strNumbers[i], out int number);
+                    if(isInt == false)
+                    {
+                        Console.WriteLine("");
+                        Console.Write("Please only enter numbers in pairs and integers : ");
+                        numbers.Clear();
+                        break;
+                    }
+                    else
+                        numbers.Add(number);
+                }
+            }
+        } while (isInt == false);
+
+        Console.WriteLine("");
+        Console.Write("Result : ");
+
+        for (int i = 0; i < numbers.Count; i +=2)
+        {
+            if(numbers[i] == numbers[i+1])
+            {
+                Console.Write(Math.Pow((numbers[i] * numbers[i+1]),2) + " ");
+            }
+            else
+            {
+                Console.Write((numbers[i] + numbers[i+1]) + " ");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Sum Of Integer Pairs - .Net Core
+In this application, the user enters *n* space-separated integer pairs and the application takes the sum if the numbers are different, square the sum if they are the same and prints it to the console.
+</br>
+
+*Sample input: 2 3 1 5 2 5 3 3* </br>
+*Sample output: 5 6 7 81*
+
+</br>
+
+[Patika.dev](https://app.patika.dev/)
